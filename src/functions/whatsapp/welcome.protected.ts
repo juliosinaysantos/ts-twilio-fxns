16827f7566647573728ff43b6273c8c70ef5208d
Julio Sinay <juliosinaysantos@outlook.com>
2021-08-31 11:49:21 -0600

    feat: add whatsapp welcome webhook

---
+++ src/functions/whatsapp/welcome.protected.ts
@@ -0,0 +1,22 @@
+import '@twilio-labs/serverless-runtime-types';
+
+import {
+  Context,
+  ServerlessCallback,
+  ServerlessFunctionSignature,
+} from '@twilio-labs/serverless-runtime-types/types';
+import { RequestParameters } from '../../parameters/RequestParameters';
+import { WhatsappParameters } from '../../parameters/WhatsappParameters';
+
+type Event = RequestParameters & WhatsappParameters
+
+export const handler: ServerlessFunctionSignature<{}, Event> = (
+  _context: Context,
+  event: Event,
+  callback: ServerlessCallback,
+) => {
+  const response = new Twilio.twiml.MessagingResponse();
+  response.message(`Welcome **${event.ProfileName ?? 'Anonymus'}** !`);
+
+  return callback(null, response.toString());
+}
